3fff0fa6fc6beb57272961373efbf4d6316d4568
alannaembury <96503327+alannaembury@users.noreply.github.com>
2022-08-21 23:32:23 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/Hvm9o0hbMMyJyG9V/scene.splinecode');
